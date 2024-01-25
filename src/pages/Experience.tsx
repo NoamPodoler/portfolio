be2ad4af24c7cd6ab41fe4c845d9e2c39685bedf
Noam Podoler <podo0noam@gmail.com>
2024-01-25 21:52:43 +0200

    no message

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -6,18 +6,18 @@ type Props = {};
 
 export default function Expereience({}: Props) {
   return (
-    <div className="w-[100vw] lg:w-full flex flex-col flex-1 justify-center items-center h-full">
-      <div className="w-[100vw] lg:w-fullflex flex-col max-w-[1200px]">
+    <div className="w-[100vw] lg:w-full xl:px-20 flex flex-col flex-1 justify-center items-center h-full">
+      <div className="w-[100vw] lg:w-full flex flex-col max-w-[1200px]">
         <h1 className="block lg:hidden mb-10 text-center">Experience</h1>
-        <div className="flex gap-10 min-w-[400px] max-h-[100vh]">
-          <div className="hidden lg:flex flex-col justify-around h-full">
+        <div className="flex h-full gap-10 min-w-[400px]">
+          <div className="hidden xl:flex flex-col justify-around h-full">
             {EXPERIENCE_DATA.map((item, index) => (
               <div key={item.company + "0"}>
                 <h3>{item.duration}</h3>
               </div>
             ))}
           </div>
-          <div className="hidden lg:flex h-full w-1 bg-stone-200 rounded-full" />
+          <div className="hidden xl:flex h-full w-1 bg-stone-200 rounded-full" />
           <div className="flex flex-col justify-around">
             {EXPERIENCE_DATA.map((item, index) => (
               <div
@@ -33,10 +33,10 @@ export default function Expereience({}: Props) {
                   <h2>
                     {item.title} | {item.company}
                   </h2>
-                  <h3 className="hidden lg:flex opacity-50 max-h-20 overflow-hidden">
+                  <h3 className="hidden lg:flex opacity-50">
                     {item.description}
                   </h3>
-                  <h3 className="flex lg:hidden opacity-50 max-h-20 overflow-hidden">
+                  <h3 className="flex lg:hidden opacity-50">
                     {item.shortDescription}
                   </h3>
                 </div>
